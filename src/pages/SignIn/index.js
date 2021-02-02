ebfb80d10f04f82ca7032b70614879dcf5829b39
Petrus Pierre <petrus.pierre@codeminer42.com>
2021-02-02 18:14:15 -0300

    SignIn and SignUp pages

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+const SignIn = () => (
+  <View style={styles.container}>
+    <Text>SignIn</Text>
+  </View>
+);
+
+export default SignIn;
---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+const SignUp = () => (
+  <View style={styles.container}>
+    <Text>SignUp</Text>
+  </View>
+);
+
+export default SignUp;
