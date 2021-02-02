95d1a5d7c05f81ea73d6d3b11c2963b94a719ff8
Petrus Pierre <petrus.pierre@codeminer42.com>
2021-02-02 17:06:00 -0300

    ESLint, Prettier and Editorconfig

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    es6: true,
+    'jest/globals': true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'jest'],
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathPrefix: '~',
+        rootPathSuffix: 'src',
+      },
+    },
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+    'react/prop-types': 'off',
+    'react/style-prop-object': 'off',
+    'react/require-default-props': 'off',
+    'import/no-mutable-exports': 'off',
+    'global-require': 'off',
+    camelcase: 'off',
+  },
+};
