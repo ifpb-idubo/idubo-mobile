5b476f523be4032ee47b4617aeda35546a463636
Petrus Pierre <petrus.pierre@codeminer42.com>
2021-02-10 16:59:12 -0300

    SignIn page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,48 +1,62 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import Login from '~/assets/login.svg';
 import { Input, Button } from '~/components';
+import { login } from '~/constants/inputs';
 
 import { metrics } from '~/theme/constants';
 import styles from './styles';
 
 const SignIn = () => {
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
+  const [inputs, setInputs] = useState([]);
 
-  const nameInput = useRef(null);
-  const passwordInput = useRef(null);
+  const navigation = useNavigation();
+
+  function handleNavigateToSignUp() {
+    navigation.navigate('SignUp');
+  }
+
+  function onChangeText(newValue, changeIndex) {
+    const inputsCopy = inputs.map((item, index) => {
+      if (index === changeIndex) {
+        return {
+          ...item,
+          value: newValue,
+        };
+      }
+      return item;
+    });
+    setInputs(inputsCopy);
+  }
+
+  useEffect(() => {
+    const inputsCopy = login.map((item, index) => ({
+      ...item,
+      index,
+    }));
+    setInputs(inputsCopy);
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Login
-        style={styles.loginTitle}
-        height={150}
-        width={metrics.width - metrics.pagePadding * 2}
-      />
-      <Input
-        reference={nameInput}
-        label="Nome"
-        value={name}
-        onChangeText={(text) => setName(text)}
-        type="TEXT"
-        next={passwordInput}
-      />
-      <Input
-        reference={passwordInput}
-        label="Senha"
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-        type="PASSWORD"
-      />
+      <Login height={150} width={metrics.titleWidth} />
+      {inputs.map((item, index) => (
+        <Input
+          value={item.value}
+          key={item.name}
+          props={item.props}
+          onChangeText={(text) => onChangeText(text, index)}
+        />
+      ))}
       <View style={styles.registerContainer}>
         <Text style={styles.inactiveText}>NÃO POSSUI UMA CONTA?</Text>
-        <TouchableOpacity onPress={() => {}}>
+        <TouchableOpacity onPress={handleNavigateToSignUp}>
           <Text style={styles.activeText}>CADASTRE-SE</Text>
         </TouchableOpacity>
       </View>
-      <Button label="ENTRAR" onPress={() => {}} enabled={name && password} />
+      <Button label="ENTRAR" onPress={() => {}} enabled />
     </View>
   );
 };
